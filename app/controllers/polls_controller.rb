0430f768f5c3c5ba6fe06f533275dd3d078b04d9
Michael Harms <michael.harms6010@gmail.com>
2020-06-18 22:51:32 -0500

    adds some notes

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,4 +1,32 @@
 require "json"
 
 class PollsController < ApplicationController
+    def show
+        @poll = Poll.find(params[:id])
+    end
+
+    def new
+        @poll = Poll.new
+    end
+
+    def create
+        @poll = Poll.new(poll_params)
+        # get taddr here?
+        if(@poll.save)
+            redirect_to @poll
+        else
+            render "new"
+        end
+    end
+
+    def destroy
+        @poll = Poll.find(params[:id])
+        @poll.destroy
+
+        redirect_to polls_path
+    end
+
+    private def poll_params
+        params.require(:poll).permit(:question, :options, :taddr)
+    end
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,7 @@
 require "json"
 
 class VotesController < ApplicationController
+
+    #These votes will be created by deposits to any taddr associated with a poll.
+    # As soon as  a new transaction comes in, it should be burned.
 end
